611d3b1d9fb1d6c4bea34e7147b215a8d6a753fe
Me4502 <mnmiller1@hotmail.com>
2017-01-21 15:03:44 +1000

    Initial Commit

---
+++ advancedserverlisticons-spigot/src/main/java/com/me4502/advancedserverlisticons/AdvancedServerListIconsSpigot.java
@@ -0,0 +1,17 @@
+package com.me4502.advancedserverlisticons;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AdvancedServerListIconsSpigot extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
